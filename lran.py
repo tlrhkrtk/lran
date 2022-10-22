6dc8712be6e2fefaaf8cd7e10ba2e5828d14459f
Αρσεν 3.6.9.01 <s.key083@gmail.com>
2022-10-22 11:45:13 +0330

    Female-Life-the-freedom

---
+++ lran.py
@@ -0,0 +1,12 @@
+from time import sleep
+count=1
+width=50
+
+for i in range(10):
+    print(("*" * count).center(width))
+    count +=2
+    sleep(0.5)
+print ("[Iran]".center(width))
+
+print("Female-Life-the-freedom" .center(width))
+print("2023".center(width))
